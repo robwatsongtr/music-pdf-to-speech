2a1f2d7dff8363b53f4df07e5e66ed53d57445d6
robwatsongtr <rwatso@gmail.com>
2025-05-19 19:35:29 -0700

    added unzip mxl

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from omr import OMR
 
 if __name__ == "__main__":
-    pachelbel = '../score_processing/pdf/Pachelbels-Canon.pdf'
+    espanoleta_score = '../score_processing/pdf/Espanoleta.pdf'
+    sor_b_min_study_score = '../score_processing/pdf/SorBminStudyOp35no22.pdf'
 
     output_path = '../score_processing/MusicXML'
 
-    # test = OMR(output_path)
-    # test.run_audiveris()
-
     pdf_to_xml1 = OMR(output_path)
-    pdf_to_xml1.run_audiveris(pachelbel)
+    pdf_to_xml1.run_audiveris(espanoleta_score)
+    pdf_to_xml1.unzip_mxl()
 
--- src/omr.py
+++ src/omr.py
@@ -1,10 +1,12 @@
+import subprocess
+import glob
+import os 
+
 """
 This class will perform optical music recognition on a pdf using Audiveris, then
 Output to MusicXML 
 """
 # ./audiveris-cli.sh -batch -export -output data data/Pachelbels-Canon.pdf
-
-import subprocess
 
 class OMR:
     def __init__(self, output_dir: str):
@@ -27,3 +29,14 @@ class OMR:
         except subprocess.CalledProcessError as e:
             print(f"Audiveris failed with exit code {e.returncode}")
 
+    def unzip_mxl(self):
+        mxl_files = glob.glob(os.path.join(self.output_dir, "*.mxl"))
+        
+        for mxl_file in mxl_files:
+            try:
+                subprocess.run(["unzip", "-o", mxl_file, "-d", self.output_dir], check=True)
+                print(f"{mxl_file} unzipped successfully")
+            except subprocess.CalledProcessError as e:
+                print(f"unzip failed with exit code {e.returncode}")
+
+        
